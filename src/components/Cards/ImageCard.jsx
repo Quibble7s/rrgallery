7ee96bbdea34a9c5917b1df5d06ad52360a6da08
Cesar Gallardo <wuelo789@gmail.com>
2021-09-24 15:07:48 -0500

    Added alt description to the images that doesnt have one

--- src/components/Cards/ImageCard.jsx
+++ src/components/Cards/ImageCard.jsx
@@ -8,14 +8,16 @@ const ImageCard = ({ img = {} }) => {
       <img
         className='card__img'
         src={`${img.urls.raw}&w=512&ar=3:4&fit=crop`}
-        alt={img.alt_description}
+        alt={img.alt_description ? img.alt_description : "img"}
+        crossOrigin='anonymous'
       />
       <div className='card-user'>
         <a href={img.user.links.html}>
           <img
             className='card-user__img --mt-small'
-            src={img.user.profile_image.large}
-            alt=''
+            src={img.user.profile_image.medium}
+            alt={img.user.first_name}
+            crossOrigin='anonymous'
           />
         </a>
         <p className='text text--color-gray text--center --mt-small --w-100 text--overflow-ellipsis'>
