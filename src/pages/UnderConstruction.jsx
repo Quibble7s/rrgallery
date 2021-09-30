8619d97893cbc6f0ac742faffa0ac6cd4bd8255c
Cesar Gallardo <wuelo789@gmail.com>
2021-09-30 00:01:43 -0500

    Added an under construction page

---
+++ src/pages/UnderConstruction.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Lottie from "react-lottie";
+import { useHistory } from "react-router-dom";
+import Button from "../components/Buttons/Button";
+
+import underConstructionAnim from "../models/animations/underconstruction_anim.json";
+
+const UnderConstruction = () => {
+  const history = useHistory();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: { ...underConstructionAnim },
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const defaultStyle = {
+    width: "100%",
+    height: "auto",
+    maxWidth: "512px",
+  };
+
+  const onClickHandler = () => {
+    history.push("/");
+  };
+
+  return (
+    <div className='container --center'>
+      <div className='--w-100 --center'>
+        <Lottie
+          style={defaultStyle}
+          options={defaultOptions}
+          isClickToPauseDisabled={true}
+        />
+        <h2 className='text text--size-subtitle text--color-gray'>
+          Page under construction
+        </h2>
+        <Button
+          onClick={onClickHandler}
+          className='btn btn--primary btn--radius-4 --mt-regular'
+          value='Go home'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UnderConstruction;
--- src/routes/UserRouter.jsx
+++ src/routes/UserRouter.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import Footer from "../components/Footer/Footer";
 import UserProfile from "../pages/UserProfile";
+
+import UnderConstruction from "../pages/UnderConstruction";
 
 const UserRouter = () => {
   return (
-    <Switch>
-      <Route exact path='/user/:id/profile' component={UserProfile} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path='/user/:id/profile' component={UnderConstruction} />
+        <Route exact path='/user/:id/liked' component={UnderConstruction} />
+        <Route exact path='/user/:id/favorites' component={UnderConstruction} />
+        <Route exact path='/user/configuration' component={UnderConstruction} />
+        <Redirect to='/' />
+      </Switch>
+      <Footer />
+    </>
   );
 };
 
