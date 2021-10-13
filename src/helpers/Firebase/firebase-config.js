8c63526353c768bfebb0c6897d3bdeb2ebd6a6fd
Cesar Gallardo <wuelo789@gmail.com>
2021-10-13 16:35:28 -0500

    e

--- src/helpers/Firebase/firebase-config.js
+++ src/helpers/Firebase/firebase-config.js
@@ -1,12 +1,5 @@
 import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAwedakHBvuoktoq7efhcDK-Gl5YHXnAtQ',
-  authDomain: 'rrgallery-7c0a0.firebaseapp.com',
-  projectId: 'rrgallery-7c0a0',
-  storageBucket: 'rrgallery-7c0a0.appspot.com',
-  messagingSenderId: '996099647694',
-  appId: '1:996099647694:web:b4520533a2962b1982c89f',
-};
+const firebaseConfig = {};
 
 export const app = initializeApp(firebaseConfig);
